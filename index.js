c432ad23e50db2c8f8fbca8db9d5c0e7573e5ad3
Njugia90 <georgecarl90@gmail.com>
2025-06-04 19:57:36 +0300

    cors added

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.port || 6655;
 //const PORT =6655;
+const cors = require("cors");
 require("./config/db");
 //require("./model/user.model");
 const userRouter = require("./router/user.router");
 
+app.use(express.json());
 app.use(express.json());
 
 app.get("/", (req, res) => {
